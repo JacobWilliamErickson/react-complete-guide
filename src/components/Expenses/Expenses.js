a0d1cc873769aa54b440905f365b49ba8d044d81
Jacob Erickson <Jakerson8@gmail.com>
2022-08-17 00:14:06 -0600

    Part 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,41 +1,31 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem"
-import ExpensesFilter from "./ExpensesFilter"
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
-const Expenses = (props) => {
-const[filteredyear,setFilteredyear] =useState('2020')
-const filterChangeHandler = selectedYear =>{
-  setFilteredyear(selectedYear);
-}
 
-return (
-<div>
-  <Card className="expenses" >
-    <ExpensesFilter selected={filteredyear} onChangeFilter={filterChangeHandler}/>
-    <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+const Expenses = (props) => {
+  const [filteredyear, setFilteredyear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredyear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredyear;
+  });
+
+  
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredyear}
+          onChangeFilter={filterChangeHandler}
         />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        />
-       <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        />
-       <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        />
-  </Card>
-</div>
-    
-)
-}
-export default Expenses
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </li>
+  );
+};
+export default Expenses;
